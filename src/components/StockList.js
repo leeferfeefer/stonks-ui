ddb2c9ada7f40f8156625df725132a2f4a827578
leeferfeefer <danbfjr@gmail.com>
2020-08-20 10:01:43 -0400

    Added infinite scroll logic, however its not the best and probably isnt the right use case. Maybe look into actually having prev page and next page buttons...

--- src/components/StockList.js
+++ src/components/StockList.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import List from '@material-ui/core/List';
@@ -16,16 +16,16 @@ const useStyles = makeStyles((theme) => ({
         justifyContent: 'center',
         alignItems: 'center',
         textAlign: 'center',
-        height: window.innerHeight-110
+        height: window.innerHeight - 110
     },
     paper: {
         display: 'flex',
         justifyContent: 'center',
-        height: window.innerHeight-110,
+        height: window.innerHeight - 110,
         width: 360
     },
-    list: {    
-        maxHeight: window.innerHeight-110,
+    list: {
+        maxHeight: window.innerHeight - 110,
         overflow: 'scroll',
     },
     spinner: {
@@ -33,59 +33,92 @@ const useStyles = makeStyles((theme) => ({
         marginTop: '50%'
     }
 }));
-  
+
 
 function StockList(props) {
     const classes = useStyles();
-    const {stonks, saveStonk, deleteStonk} = props;
-    const [stockSymbols, setStockSymbols] = useState([]);
+    const { stonks, saveStonk, deleteStonk } = props;
+    let [stockSymbols, setStockSymbols] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [page, setPage] = useState(0);
-    const [prevY, setPrevY] = useState(0);
+    let [page, setPage] = useState(0);
 
     // runs both after the first render and after every update.
     useEffect(() => {
-        const getStockSymbols = async () => {            
-            const stockSymbols = await StockSymbolsService.getStockSymbols(page);
+        const getStockSymbols = async () => {
+            const stockSymbolsResponse = await StockSymbolsService.getStockSymbols(page);
             setLoading(false);
-            setStockSymbols(stockSymbols);
+            setStockSymbols([...stockSymbols, ...stockSymbolsResponse]);
         }
         getStockSymbols();
     }, [page]); // pass [] so that it doesnt re-render after getting stock symbols and setting state
 
 
+    // INFINITE SCROLL LOGIC
+    // --------------------------------------------------
+    const [bottom, setBottom] = React.useState(null);
+    const bottomObserver = React.useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            entries => {
+                const entry = entries[0];
+                if (entry.isIntersecting) {
+                    setPage(++page);
+                }
+            },
+            { threshold: 0.25, rootMargin: "50px" }
+        );
+        bottomObserver.current = observer;
+    }, []);
+
+    React.useEffect(() => {
+        const observer = bottomObserver.current;
+        if (bottom) {
+            observer.observe(bottom);
+        }
+        return () => {
+            if (bottom) {
+                observer.unobserve(bottom);
+            }
+        };
+    }, [bottom]);
+
+    // --------------------------------------------------
+
     const renderRow = () => {
-        return stockSymbols.map((stonk, index) => 
+        return stockSymbols.map((stonk, index) =>
             <React.Fragment key={`${index}-divider`}>
-                <StockListItem 
-                    key={index} 
-                    stonk={stonk} 
+                <StockListItem
+                    key={index}
+                    stonk={stonk}
                     saveStonk={saveStonk}
                     deleteStonk={deleteStonk}
                 />
-                <Divider/>
+                <Divider />
             </React.Fragment>
         );
     };
     return (
         <Paper className={classes.paper}>
-            {loading && <CircularProgress className={classes.spinner}/>}
-            {stockSymbols.length === 0 ?            
+            {loading && <CircularProgress className={classes.spinner} />}
+            {stockSymbols.length === 0 ?
                 <div className={classes.listLabel}>
                     Stonk List
-                </div>                 
-            :                
-                <List
-                    className={classes.list}
-                    component="ul"
-                    subheader={
-                        <ListSubheader component="div">
-                            Stonk List
-                        </ListSubheader>
-                    }
-                >           
-                    {renderRow()}    
-                </List>         
+                </div>
+                :
+                <div className={classes.list}>
+                    <List
+                        component="ul"
+                        subheader={
+                            <ListSubheader component="div">
+                                Stonk List
+                            </ListSubheader>
+                        }
+                    >
+                        {renderRow()}
+                    </List>
+                    <div ref={setBottom}>loading...</div>
+                </div>
             }
         </Paper>
     );
